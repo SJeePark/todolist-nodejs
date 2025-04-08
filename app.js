0af01797fd74d61f45f31ecec30e12e808e3395b
SangJee Park <sjpark3927@gmail.com>
2025-04-09 00:45:27 +0900

    deploy aws

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const indexRouter = require("./routes/index");
 const cors = require('cors');
+require('dotenv').config()
 
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+console.log("mongouri", MONGODB_URI_PROD);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+const mongoURI = MONGODB_URI_PROD
 
 mongoose.connect(mongoURI, { useNewUrlParser: true })
     .then(() => {
@@ -20,6 +23,6 @@ mongoose.connect(mongoURI, { useNewUrlParser: true })
         console.log("DB connection fail", err);
     });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log("server on 5000");
 });
